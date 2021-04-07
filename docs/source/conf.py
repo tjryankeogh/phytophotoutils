34bd2ed5b071df49b3ab61c1c34ccbf749cf9429
Tommy Ryan-Keogh <tjryankeogh@googlemail.com>
2021-04-07 17:09:18 +0200

    doc fix

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -15,10 +15,10 @@
 import sys
 import os
 
-#sys.path.insert(0, os.path.abspath('..'))
-#sys.path.insert(0, os.path.abspath('../..'))
+sys.path.insert(0, os.path.abspath('..'))
+sys.path.insert(0, os.path.abspath('../..'))
 sys.path.insert(0, os.path.realpath('../..'))
-#sys.path.insert(0, os.path.realpath('.pandas'))
+sys.path.insert(0, os.path.realpath('.pandas'))
 
 # -- Project information -----------------------------------------------------
 
